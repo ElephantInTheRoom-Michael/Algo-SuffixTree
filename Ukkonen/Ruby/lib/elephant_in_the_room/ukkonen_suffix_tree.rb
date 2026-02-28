a5b4cf8f976f841a0a560df020a0aa4385373532
Michael Taylor <241386356+ElephantInTheRoom-Michael@users.noreply.github.com>
2026-02-27 21:04:02 -0800

    Initial gem creation

---
+++ Ukkonen/Ruby/lib/elephant_in_the_room/ukkonen_suffix_tree.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "ukkonen_suffix_tree/version"
+
+module ElephantInTheRoom
+  module UkkonenSuffixTree
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ Ukkonen/Ruby/spec/elephant_in_the_room/ukkonen_suffix_tree_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ElephantInTheRoom::UkkonenSuffixTree do
+  it "has a version number" do
+    expect(ElephantInTheRoom::UkkonenSuffixTree::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
