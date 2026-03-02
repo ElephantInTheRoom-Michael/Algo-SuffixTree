56342ab87aacccb9b019ed289c72b8afd234a2b6
Michael Taylor <241386356+ElephantInTheRoom-Michael@users.noreply.github.com>
2026-03-01 20:07:02 -0800

    Introduce the active point and start by adding a node per character

--- Ukkonen/Ruby/lib/elephant_in_the_room/ukkonen_suffix_tree/ukkonen_suffix_tree.rb
+++ Ukkonen/Ruby/lib/elephant_in_the_room/ukkonen_suffix_tree/ukkonen_suffix_tree.rb
@@ -3,24 +3,76 @@
 module ElephantInTheRoom
   module UkkonenSuffixTree
     class UkkonenSuffixTree
-      attr_reader :letters
+      class Node
+        attr_reader :edges
+
+        def initialize
+          @edges = {}
+        end
+
+        def add_edge(letter, start)
+          @edges[letter] = Edge.new(start)
+        end
+      end
+
+      class Edge
+        attr_reader :start
+
+        def initialize(start)
+          @start = start
+        end
+      end
+
+      class InnerEdge < Edge
+        attr_reader :to
+
+        def initialize(to)
+          @to = to
+        end
+      end
 
       def initialize
         @letters = []
+
+        @root = Node.new
+        @active_node = @root
+        @active_edge = nil
+        @active_length = 0
       end
 
       def add(text)
-        text.each_char { |letter| add_letter(letter) }
+        text.each_char { add_letter(_1) }
       end
 
-      def contains(text)
-        @letters.join == text
+      def contains?(text)
+        search(text, false)
+      end
+
+      def ends_with?(text)
+        search(text, true)
       end
 
       private
 
       def add_letter(letter)
         @letters << letter
+
+        @active_node.add_edge(letter, @letters.length - 1)
+      end
+
+      def search(text, must_be_suffix)
+        edge = @root.edges[text[0]]
+        return false unless edge
+        return false if must_be_suffix && edge.is_a?(InnerEdge)
+        ends_at = edge.start
+        text.each_char do |l|
+          if @letters[ends_at] == l
+            ends_at += 1
+          else
+            return false
+          end
+        end
+        !must_be_suffix || ends_at == @letters.length
       end
     end
   end
--- Ukkonen/Ruby/spec/elephant_in_the_room/ukkonen_suffix_tree/ukkonen_suffix_tree_spec.rb
+++ Ukkonen/Ruby/spec/elephant_in_the_room/ukkonen_suffix_tree/ukkonen_suffix_tree_spec.rb
@@ -3,8 +3,43 @@
 require 'rspec'
 
 RSpec.describe 'UkkonenSuffixTree' do
-  it 'checks for a full match' do
-    expect(ElephantInTheRoom::UkkonenSuffixTree.from("text").contains("text")).to be true
-    expect(ElephantInTheRoom::UkkonenSuffixTree.from("text").contains("word")).to be false
+  context 'when the text has no repeated characters' do
+    it 'checks for a full match' do
+      tree = ElephantInTheRoom::UkkonenSuffixTree.from("abc")
+      expect(tree.contains?("abc")).to be true
+      expect(tree.ends_with?("abc")).to be true
+      expect(tree.contains?("xyz")).to be false
+      expect(tree.ends_with?("xyz")).to be false
+    end
+
+    it 'checks single character matches' do
+      tree = ElephantInTheRoom::UkkonenSuffixTree.from("abc")
+      expect(tree.contains?("a")).to be true
+      expect(tree.contains?("b")).to be true
+      expect(tree.contains?("c")).to be true
+      expect(tree.contains?("d")).to be false
+      expect(tree.ends_with?("a")).to be false
+      expect(tree.ends_with?("b")).to be false
+      expect(tree.ends_with?("c")).to be true
+      expect(tree.ends_with?("d")).to be false
+    end
+
+    it 'checks small substrings' do
+      tree = ElephantInTheRoom::UkkonenSuffixTree.from("abcd")
+      expect(tree.contains?("ab")).to be true
+      expect(tree.contains?("bc")).to be true
+      expect(tree.contains?("cd")).to be true
+      expect(tree.contains?("ae")).to be false
+      expect(tree.contains?("be")).to be false
+      expect(tree.contains?("de")).to be false
+      expect(tree.contains?("ef")).to be false
+      expect(tree.ends_with?("ab")).to be false
+      expect(tree.ends_with?("bc")).to be false
+      expect(tree.ends_with?("cd")).to be true
+      expect(tree.ends_with?("ae")).to be false
+      expect(tree.ends_with?("be")).to be false
+      expect(tree.ends_with?("de")).to be false
+      expect(tree.ends_with?("ef")).to be false
+    end
   end
 end
