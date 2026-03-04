689461ed431513d8256e7c0019902ef9e95a0145
Michael Taylor <241386356+ElephantInTheRoom-Michael@users.noreply.github.com>
2026-03-03 22:08:19 -0800

    Extend algorithm to handle simple repeated characters

--- Ukkonen/Ruby/lib/elephant_in_the_room/ukkonen_suffix_tree/ukkonen_suffix_tree.rb
+++ Ukkonen/Ruby/lib/elephant_in_the_room/ukkonen_suffix_tree/ukkonen_suffix_tree.rb
@@ -15,6 +15,8 @@ module ElephantInTheRoom
         end
       end
 
+      class EndNode < Node; end
+
       class Edge
         attr_reader :start
 
@@ -25,9 +27,12 @@ module ElephantInTheRoom
 
       class InnerEdge < Edge
         attr_reader :to
+        attr_reader :node
 
-        def initialize(to)
+        def initialize(start, to, node)
+          super(start)
           @to = to
+          @node = node
         end
       end
 
@@ -38,18 +43,25 @@ module ElephantInTheRoom
         @active_node = @root
         @active_edge = nil
         @active_length = 0
+        @remainder = 0
       end
 
       def add(text)
         text.each_char { add_letter(_1) }
       end
 
+      def finalize
+        if @remainder > 0
+          split_active_edge(EndNode.new)
+        end
+      end
+
       def contains?(text)
-        search(text, false)
+        search(text, @root, false)
       end
 
       def ends_with?(text)
-        search(text, true)
+        search(text, @root, true)
       end
 
       private
@@ -57,22 +69,43 @@ module ElephantInTheRoom
       def add_letter(letter)
         @letters << letter
 
-        @active_node.add_edge(letter, @letters.length - 1)
+        @remainder += 1
+
+        if @active_edge
+          if letter == @letters[@active_edge.start + @active_length]
+            @active_length += 1
+          else
+            # Split edge
+          end
+        elsif @active_node.edges.has_key?(letter)
+          @active_edge = @active_node.edges[letter]
+          @active_length = 1
+        else
+          @active_node.add_edge(letter, @letters.length - 1)
+          @remainder = 0
+        end
       end
 
-      def search(text, must_be_suffix)
-        edge = @root.edges[text[0]]
+      def split_active_edge(inner_node)
+        right_edge = Edge.new(@active_edge.start + @active_length)
+        left_edge = InnerEdge.new(@active_edge.start, right_edge.start, inner_node)
+        @active_node.edges[@letters[left_edge.start]] = left_edge
+        inner_node.edges[@letters[right_edge.start]] = right_edge
+      end
+
+      def search(text, node, must_be_suffix)
+        return true if text.empty? && (!must_be_suffix || node.is_a?(EndNode))
+        edge = node.edges[text[0]]
         return false unless edge
-        return false if must_be_suffix && edge.is_a?(InnerEdge)
-        ends_at = edge.start
-        text.each_char do |l|
-          if @letters[ends_at] == l
-            ends_at += 1
-          else
-            return false
-          end
+        start = edge.start
+        to = edge.is_a?(InnerEdge) ? edge.to : @letters.length
+        return false if (to - start) > text.length && must_be_suffix
+        (0...text.length).each do |i|
+          return false if i == to - start && !edge.is_a?(InnerEdge)
+          return search(text[i..], edge.node, must_be_suffix) if i == to - start
+          return false if @letters[start + i] != text[i]
         end
-        !must_be_suffix || ends_at == @letters.length
+        true
       end
     end
   end
--- Ukkonen/Ruby/spec/elephant_in_the_room/ukkonen_suffix_tree/ukkonen_suffix_tree_spec.rb
+++ Ukkonen/Ruby/spec/elephant_in_the_room/ukkonen_suffix_tree/ukkonen_suffix_tree_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'UkkonenSuffixTree' do
   context 'when the text has no repeated characters' do
     it 'checks for a full match' do
       tree = ElephantInTheRoom::UkkonenSuffixTree.from("abc")
+      tree.finalize
       expect(tree.contains?("abc")).to be true
       expect(tree.ends_with?("abc")).to be true
       expect(tree.contains?("xyz")).to be false
@@ -14,6 +15,7 @@ RSpec.describe 'UkkonenSuffixTree' do
 
     it 'checks single character matches' do
       tree = ElephantInTheRoom::UkkonenSuffixTree.from("abc")
+      tree.finalize
       expect(tree.contains?("a")).to be true
       expect(tree.contains?("b")).to be true
       expect(tree.contains?("c")).to be true
@@ -26,6 +28,7 @@ RSpec.describe 'UkkonenSuffixTree' do
 
     it 'checks small substrings' do
       tree = ElephantInTheRoom::UkkonenSuffixTree.from("abcd")
+      tree.finalize
       expect(tree.contains?("ab")).to be true
       expect(tree.contains?("bc")).to be true
       expect(tree.contains?("cd")).to be true
@@ -40,6 +43,19 @@ RSpec.describe 'UkkonenSuffixTree' do
       expect(tree.ends_with?("be")).to be false
       expect(tree.ends_with?("de")).to be false
       expect(tree.ends_with?("ef")).to be false
+    end
+  end
+
+  context "when the text has repeated characters" do
+    it 'handles a simple one character repetition' do
+      tree = ElephantInTheRoom::UkkonenSuffixTree.from("abca")
+      tree.finalize
+      expect(tree.contains?("a")).to be true
+      expect(tree.contains?("ab")).to be true
+      expect(tree.contains?("ca")).to be true
+      expect(tree.ends_with?("a")).to be true
+      expect(tree.ends_with?("ca")).to be true
+      expect(tree.ends_with?("abca")).to be true
     end
   end
 end
